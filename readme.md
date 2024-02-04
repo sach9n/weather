8d880fa68ac58b4cd73a32f8eaff309700601d54
sach9n <66356517+sach9n@users.noreply.github.com>
2024-02-04 18:23:38 +0530

    initialCommit

---
+++ readme.md
@@ -0,0 +1,6 @@
+completed a weather app which show Real time weather
+
+learning
+
+Weather fetching from the openWeather 
+CSS and HTml improved
---
+++ script.js
@@ -0,0 +1,48 @@
+const ApiKey = "c9fe059483dae96a500302a98a41c335";
+const ApiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchVal = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search span')
+const weatherIcon = document.querySelector('.weather-icon');
+async function checkWeather(city){
+  const response = await fetch(ApiUrl + city + `&appid=${ApiKey}`);
+   
+  if(response.status == 404){
+    document.querySelector('.error').style.display = 'block'
+    document.querySelector('.weather').style.display  = "none";
+  }else {
+    let data = await response.json();
+
+   
+  document.querySelector('.city').innerHTML = data.name;
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "&degC";
+  document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+  document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
+  
+  
+
+  if(data.weather[0].main == "Clouds"){
+    weatherIcon.src = 'images/clouds.png';
+  }else if(data.weather[0].main == 'Clear'){
+    weatherIcon.src = 'images/clear.png'
+  }else if(data.weather[0].main == 'Rain'){
+    weatherIcon.src = 'images/rain.png'
+  }
+  else if(data.weather[0].main == 'Drizzle'){
+    weatherIcon.src = 'images/drizzle.png'
+  }
+  else if(data.weather[0].main == 'Mist'){
+    weatherIcon.src = 'images/mist.png'
+  }
+  
+  
+
+  document.querySelector('.weather').style.display  = "block";
+  document.querySelector('.error').style.display = 'none'
+     
+}
+}
+
+ searchBtn.addEventListener('click',()=>{
+      checkWeather(searchVal.value);
+ })
+
